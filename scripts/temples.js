a8aea0909cebdfbe802df982eb5d939ecad81b23
Fatima Puro <122589022+Fatimapuro-byui@users.noreply.github.com>
2025-11-08 19:06:29 -0400

    Update temples.js

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,1 +1,25 @@
+const yearElement = document.getElementById('year');
+const lastModifiedElement = document.getElementById('last-modified');
 
+yearElement.textContent = new Date().getFullYear();
+lastModifiedElement.textContent = new Date(document.lastModified).toLocaleString('en-US', {
+  year: 'numeric',
+  month: '2-digit',
+  day: '2-digit',
+  hour: '2-digit',
+  minute: '2-digit'
+});
+
+
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+
+hamburger.addEventListener('click', () => {
+  if (navMenu.style.display === 'flex') {
+    navMenu.style.display = 'none';
+    hamburger.textContent = '☰'; 
+  } else {
+    navMenu.style.display = 'flex';
+    hamburger.textContent = '✕'; 
+  }
+});
